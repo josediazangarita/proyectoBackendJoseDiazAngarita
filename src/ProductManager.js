d61a3d8726ababba2196400d34d745a132e89bde
José Gregorio Díaz Angarita <josediazangarita@MacBook-Air-de-Jose.local>
2024-03-06 17:23:38 -0300

    Se agrega gitignore, se reorganizan archivos segun el requerimiento del desafio, se agrega la logica de app.js y se importa ProductManager. Servidor activo con las rutas y sus especificaciones solicitadas en el desafio.

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -1,12 +1,12 @@
 //Script del desafío entregable dos del curso Backend de Coderhouse
-console.log("\nSegundo desafío entregable Backend de José Gregorio Díaz Angarita\n")
+console.log("\nTercer desafío entregable Backend de José Gregorio Díaz Angarita\n")
 
 //Se importa el módulo de FuleSystem para manipular archivos
-const fs = require('fs');
+import { readFileSync, writeFileSync } from 'fs';
 
-class ProductManager {
+export default class ProductManager {
     constructor() {
-        this.path = 'products.json';
+        this.path = '../src/products.json';
         this.products = [];
         this.productIdCounter = 1;
         this.loadProducts();
@@ -38,11 +38,12 @@ class ProductManager {
 
     //Método para obtener un producto almacenado por su ID
     getProductById(id) {
-        const product = this.products.find(p => p.id === id);
+        const productId = parseInt(id); // Convertir el ID a entero
+        const product = this.products.find(p => p.id === productId);
         if (!product) {
-            console.log(`Error: No se encuentra el producto con el ID ${id}.\n`);
+            console.log(`Error: No se encuentra el producto con el ID ${productId}.\n`);
         } else {
-            console.log(`El producto con ID ${id} es el siguiente:`, product);
+            console.log(`El producto con ID ${productId} es el siguiente:`, product);
             console.log('\n');
         }
         return product;
@@ -50,7 +51,7 @@ class ProductManager {
 
     async loadProducts() {
         try {
-            const data = fs.readFileSync(this.path, 'utf8');
+            const data = readFileSync(this.path, 'utf8');
             this.products = JSON.parse(data);
             this.productIdCounter = Math.max(...this.products.map(product => product.id), 0) + 1;
         } catch (error) {
@@ -61,7 +62,7 @@ class ProductManager {
     async saveProducts() {
         try {
             const data = JSON.stringify(this.products, null, 2);
-            fs.writeFileSync(this.path, data);
+            writeFileSync(this.path, data);
         } catch (error) {
             console.error('Error al guardar productos en el archivo:', error.message);
         }
@@ -96,79 +97,3 @@ class ProductManager {
         console.log(`El producto con ID ${id} fue eliminado correctamente.\n`)
     }
 }
-
-//Proceso de testing 
-
-const store = new ProductManager();
-
-console.log(store);
-
-//Test agregar productos correctamente
-store.addProduct({
-    title: "Producto 1",
-    description: "descripción del producto 1",
-    price: 100,
-    thumbnail: "imagen1.jpg",
-    code: "001",
-    stock: 100
-});
-
-store.addProduct({
-    title: "Producto 2",
-    description: "descripción del producto 2",
-    price: 200,
-    thumbnail: "imagen2.jpg",
-    code: "002",
-    stock: 200
-});
-
-store.addProduct({
-    title: "Producto 3",
-    description: "descripción del producto 3",
-    price: 300,
-    thumbnail: "imagen3.jpg",
-    code: "003",
-    stock: 300
-});
-
-//Obtener la lista de todos los productos
-store.getProducts();
-
-//Test agregar un producto con campo faltante
-store.addProduct({
-    title: "Producto 4",
-    description: "descripción del producto 4",
-    price: 400,
-    //thumbnail: "imagen4.jpg",
-    code: "004",
-    stock: 400
-});
-
-//Test agregar un producto con código existente
-store.addProduct({
-    title: "Producto 5",
-    description: "descripción del producto 5",
-    price: 500,
-    thumbnail: "imagen5.jpg",
-    code: "001",
-    stock: 500
-});
-
-//Obtener productos por su ID
-store.getProductById(2);
-store.getProductById(3);
-
-//Obtener producto con un ID inexistente (error)
-store.getProductById(20);
-
-//Actualizar campo de un producto
-store.updateProduct(2, { price: 1000 });
-
-//Error al intentar cambiar el campo id de un producto
-store.updateProduct(2, { id: 10 });
-
-//Test de eliminación de un producto por id
-store.deleteProduct(3);
-
-//Test de eliminación de un producto cin id inexistente (error)
-store.deleteProduct(15);
