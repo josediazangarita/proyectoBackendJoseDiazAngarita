65952e3555a26bee39b7a42724cd3604bb64ec60
josediazangarita <josediazangarita@gmail.com>
2024-04-27 09:31:20 -0300

    Se agrega modelo de usuario user.

---
+++ src/dao/models/user.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userCollection = "users";
+
+const userSchema = new mongoose.Schema({
+    first_name: String,
+    last_name: String,
+    email: {
+        type: String,
+        unique: true
+    }
+});
+
+export const UserModel = mongoose.model(userCollection, userSchema);
