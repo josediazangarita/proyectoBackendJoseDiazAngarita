5cba1972411328e9888892afad56945692036dfa
josediazangarita <josediazangarita@gmail.com>
2024-04-27 09:58:55 -0300

    Se agrega usersRouter a carpeta de rutas

---
+++ src/routes/usersRouter.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { userModel } from '../models/userModel.js';
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+    try {
+        let users = await userModel.find()
+        res.send({ resul: "seccess", payload: users })
+    }
+    catch (error) {
+        console.log("No se pueden obtener usuarios con mongoose:" + error)
+        res.status(500).send({ status: "error", message: error.message })
+    }
+});
