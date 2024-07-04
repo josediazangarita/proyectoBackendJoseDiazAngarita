5dbf0bb405a3670cc1257926a7cfe1b57a539c69
josediazangarita <josediazangarita@gmail.com>
2024-07-04 12:08:32 -0300

    Se agregan los campos Id y purchase_datetime a la vista ticket.

--- src/dto/ticketDTO.js
+++ src/dto/ticketDTO.js
@@ -1,5 +1,6 @@
 class TicketDTO {
-    constructor({ code, purchase_datetime, amount, purchaser }) {
+    constructor({ _id, code, purchase_datetime, amount, purchaser }) {
+        this._id = _id;
         this.code = code;
         this.purchaseDatetime = purchase_datetime;
         this.amount = amount;
